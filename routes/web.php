bcb471934942a396bfe4db9f808671e4ed998101
evgeny <evgeny@mail.ru>
2017-04-21 15:38:37 +0300

    Подключение шаблона

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@
  * ячейка uses хранит инф. о том какой контроллер,
  * обработает запрос
  */
+
 Route::match(['get', 'post'], '/', ['uses'=>'IndexController@execute', 'as'=>'home']);
 
 /*
@@ -28,22 +29,22 @@ Route::get('/page/{alias}', ['uses'=>'PageController@execute', 'as'=>'page'] );
 /*
  * Формирование маршрута для аутентификации
  */
-Route::auth();
+ Route::auth();
 
 /*
  * Группа маршрутов для закрытого раздела
  * данного проекта
  * например admin/page, admin/portfolio и тд
  */
-Route::group(['group'=>'admin', 'middleware'=>'auth'], function(){
+//Route::group(['group'=>'admin', 'middleware'=>'auth'], function(){
 
    /*
     * Маршрут главн. стр. панели администрации
     * /admin
     */
-   Route::get('/', function(){
+   //Route::get('/', function(){
 
-   });
+  // });
 
    /*
     * Группа маршрутов для работы с страницами, т.е. с инф.
@@ -51,56 +52,58 @@ Route::group(['group'=>'admin', 'middleware'=>'auth'], function(){
     * /admin/pages
     *
     */
-   Route::group(['prefix'=>'pages'], function(){
+ //  Route::group(['prefix'=>'pages'], function(){
       /*
        * Главн. стр раздела по управлению стр.
        */
-      Route::get('/', ['uses'=>'PagesController@execute', 'as'=>'pages']);
+ //     Route::get('/', ['uses'=>'PagesController@execute', 'as'=>'pages']);
 
       //admin/pages/add
-      Route::match(['get', 'post'], '/add', ['uses'=>'PagesAddController@execute', 'as'=>'pagesAdd']);
+  //    Route::match(['get', 'post'], '/add', ['uses'=>'PagesAddController@execute', 'as'=>'pagesAdd']);
       //admin/edit/2
-      Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses'=>'PagesEditController@execute', 'as'=>'pagesEdit']);
-   });
+    //  Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses'=>'PagesEditController@execute', 'as'=>'pagesEdit']);
+ //  });
 
    /*
     * Маршруты для работы с инф. которая хранится в
     * табл. portfolios, т.е. для редактирования, дбавления, удаленя
     * портфолио
     */
-   Route::group(['prefix'=>'portfolios'], function(){
+ //  Route::group(['prefix'=>'portfolios'], function(){
 
       /*
        * Главн. стр раздела портфолио
        */
-      Route::get('/', ['uses'=>'PortfolioController@execute', 'as'=>'portfolio']);
+  //    Route::get('/', ['uses'=>'PortfolioController@execute', 'as'=>'portfolio']);
 
       /*
        * маршруты для реализации какого либо действия
        */
-      Route::match(['get', 'post'], '/add', ['uses'=>'PortfolioAddController@execute', 'as'=>'portfolioAdd']);
+   //   Route::match(['get', 'post'], '/add', ['uses'=>'PortfolioAddController@execute', 'as'=>'portfolioAdd']);
 
-      Route::match(['get', 'post', 'delete'], '/edit/{portfolio}', ['uses'=>'PortfolioEditController@execute', 'as'=>'portfolioEdit']);
-   });
+   //   Route::match(['get', 'post', 'delete'], '/edit/{portfolio}', ['uses'=>'PortfolioEditController@execute', 'as'=>'portfolioEdit']);
+ //  });
 
    /*
     * Маршруты для работы с инф. которая хранится в
     * табл. services, т.е. для редактирования, дбавления, удаленя
     * сервиса
     */
-   Route::group(['prefix'=>'services'], function(){
+ //  Route::group(['prefix'=>'services'], function(){
 
       /*
        * Главн. стр раздела сервиса
        */
-      Route::get('/', ['uses'=>'ServiceController@execute', 'as'=>'services']);
+   //   Route::get('/', ['uses'=>'ServiceController@execute', 'as'=>'services']);
 
       /*
        * маршруты для реализации какого либо действия
        */
-      Route::match(['get', 'post'], '/add', ['uses'=>'ServiceAddController@execute', 'as'=>'serviceAdd']);
+  //    Route::match(['get', 'post'], '/add', ['uses'=>'ServiceAddController@execute', 'as'=>'serviceAdd']);
 
-      Route::match(['get', 'post', 'delete'], '/edit/{service}', ['uses'=>'ServiceEditController@execute', 'as'=>'serviceEdit']);
-   });
-});
+   //   Route::match(['get', 'post', 'delete'], '/edit/{service}', ['uses'=>'ServiceEditController@execute', 'as'=>'serviceEdit']);
+ //  });
+//});
 
+
+//Route::get('/home', 'HomeController@index');
