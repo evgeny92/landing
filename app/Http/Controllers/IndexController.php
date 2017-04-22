94a6704acc93c76af62b7bb5c3afd9650e57d4dc
evgeny <evgeny@mail.ru>
2017-04-22 12:59:33 +0300

    Отображение выбранной информации на экран из бд, первых двух секции лэндинга

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -20,7 +20,37 @@ class IndexController extends Controller
        $peoples =People::all();
 
 
-       return view('site.index');
+       //будет сожержать список меню
+       $menu = array();
+       //циклом выводим меню
+       foreach($pages as $page){
+          //отдельный пункт меню
+         $item = array('title'=>$page->name, 'alias'=>$page->alias);
+          //добавим отдельный пункт меню в массив $menu
+          array_push($menu, $item);
+       }
+
+       $item = array('title'=>'Services', 'alias'=>'service');
+       array_push($menu, $item);
+
+       $item = array('title'=>'Portfolio', 'alias'=>'Portfolio');
+       array_push($menu, $item);
+
+       $item = array('title'=>'Team', 'alias'=>'team');
+       array_push($menu, $item);
+
+       $item = array('title'=>'Contact', 'alias'=>'contact');
+       array_push($menu, $item);
+
+       return view('site.index', array(
+                              //ключи имена переменных
+                              //эти переменные будут доступны в шаблоне
+                               'menu'=>$menu,
+                               'pages'=>$pages,
+                               'services'=>$services,
+                               'portfolios'=>$portfolios,
+                               'peoples'=>$peoples,
+                            ));
 
     }
 }
